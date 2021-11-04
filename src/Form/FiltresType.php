afca77ff6f6cc3b939e4ee51dddd491a8fcf11cd
coralineENI <89263375+coralineENI@users.noreply.github.com>
2021-11-04 09:20:02 +0100

    FiltresType

---
+++ src/Form/FiltresType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Site;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FiltresType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('site', EntityType::class, [
+                'class' => Site::class,
+                'choice_label' => 'nom',
+                'placeholder' => ''
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Le nom de la sortie contient :',
+                'required' => false
+            ])
+            ->add('debut', DateTimeType::class, [
+                'label' => 'Entre :',
+                'date_widget' => 'single_text',
+                'html5' => true,
+            ])
+            ->add('fin', DateTimeType::class, [
+                'label' => 'et :',
+                'date_widget' => 'single_text',
+                'html5' => true,
+            ])
+            ->add('organisateur', CheckboxType::class, [
+                'label' => 'Sortie dont je suis l\'organisateur/trice',
+                'required' => false,
+
+            ])
+            ->add('inscrit', CheckboxType::class, [
+                'label' => 'Sortie auxquelles je suis inscrit/e',
+                'required' => false,
+
+            ])
+            ->add('pasInscrit', CheckboxType::class, [
+                'label' => 'Sortie auxquelles je ne suis pas inscrit/e',
+                'required' => false,
+
+            ])
+            ->add('sortiePassee', CheckboxType::class, [
+                'label' => 'Sorties passées',
+                'required' => false,
+
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}

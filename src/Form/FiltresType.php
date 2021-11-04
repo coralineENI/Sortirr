43e49093b472ebc2cb63ffcca33615663fcb84b3
coralineENI <89263375+coralineENI@users.noreply.github.com>
2021-11-04 13:58:09 +0100

    Archives fonctionnent

--- src/Form/FiltresType.php
+++ src/Form/FiltresType.php
@@ -19,22 +19,22 @@ class FiltresType extends AbstractType
             ->add('site', EntityType::class, [
                 'class' => Site::class,
                 'choice_label' => 'nom',
+                'required'=>false,
                 'placeholder' => ''
             ])
             ->add('nom', TextType::class, [
-                'label' => 'Le nom de la sortie contient :',
-                'required' => false
+                'label' => 'Le nom de la sortie contient :'
             ])
-            ->add('debut', DateTimeType::class, [
-                'label' => 'Entre :',
-                'date_widget' => 'single_text',
-                'html5' => true,
-            ])
-            ->add('fin', DateTimeType::class, [
-                'label' => 'et :',
-                'date_widget' => 'single_text',
-                'html5' => true,
-            ])
+//            ->add('debut', DateTimeType::class, [
+//                'label' => 'Entre :',
+//                'date_widget' => 'single_text',
+//                'html5' => true,
+//            ])
+//            ->add('fin', DateTimeType::class, [
+//                'label' => 'et :',
+//                'date_widget' => 'single_text',
+//                'html5' => true,
+//            ])
             ->add('organisateur', CheckboxType::class, [
                 'label' => 'Sortie dont je suis l\'organisateur/trice',
                 'required' => false,
@@ -56,12 +56,19 @@ class FiltresType extends AbstractType
 
             ])
         ;
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'method'=> 'get',
+            'csrf_protection'=> false
         ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
     }
 }
